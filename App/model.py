a0f1194ca4c2adc52406aa6e2eb734579fb9faca
Samuel <s.torresf@uniandes.edu.co>
2021-02-17 22:10:26 -0500

    adding models and controller

--- App/model.py
+++ App/model.py
@@ -36,13 +36,100 @@ los mismos.
 """
 
 # Construccion de modelos
+def newCatalog():
+    """
+    Inicializa el catálogo de libros. Crea una lista vacia para guardar
+    todos los libros, adicionalmente, crea una lista vacia para los autores,
+    una lista vacia para los generos y una lista vacia para la asociación
+    generos y libros. Retorna el catalogo inicializado.
+    """
+    catalog = {'videos':None,
+               'trending_date': None,
+               'title': None,
+               'channel_title': None,
+               'publish_time': None,
+               'views': None,
+               'likes': None,
+               'dislikes': None}
+
+    catalog['videos'] = lt.newList()
+    catalog['trending_date'] = lt.newList()
+    catalog['title'] = lt.newList()
+    catalog['channel_title'] = lt.newList()
+    catalog['publish_time'] = lt.newList()
+    catalog['views'] = lt.newList('SINGLE_LINKED',
+                                    cmpfunction=compareviews)
+    catalog['likes'] = lt.newList()
+    catalog['dislikes'] = lt.newList()
+
+    return catalog
 
 # Funciones para agregar informacion al catalogo
 
+def addVideo(catalog, video):
+    lt.addLast(catalog['videos'], video)
+    # Se obtienen los autores del libro
+    trending_dates = video['trending_date'].split(",")
+    titles = video['title'].split(",")
+    channel_titles = video['channel_title'].split(",")
+    publish_times = video['publish_time'].split(",")
+    views = video['views'].split(",")
+    likes = video['likes'].split(",")
+    dislikes = video['dislikes'].split(",")
+    # Cada autor, se crea en la lista de libros del catalogo, y se
+    # crea un libro en la lista de dicho autor (apuntador al libro)
+    for channel_title in channel_titles:
+        addChannel(catalog, author.strip(), book)
+
+def addChannel(catalog, channelname, video):
+    """
+    Adiciona un autor a lista de autores, la cual guarda referencias
+    a los libros de dicho autor
+    """
+    channels = catalog['channel_title']
+    poschannel = lt.isPresent(channels, channelname)
+    if poschannel > 0:
+        channel = lt.getElement(channels, poschannel)
+    else:
+        channel = newChannel(channelname)
+        lt.addLast(channels, channel)
+    lt.addLast(author['books'], book)
+
+
+def addID(catalog, id):
+    """compareratings
+    Adiciona un tag a la lista de tags
+    """
+    t = newID(id['name'], tagid['id'])
+    lt.addLast(catalog['ids'], t)
+
 # Funciones para creacion de datos
+
+def newChannel(name):
+    """
+    Crea una nueva estructura para modelar los libros de
+    un autor y su promedio de ratings
+    """
+    channel = {'name': "", "videos": None,  "likes": 0}
+    channel['name'] = name
+    channel['videos'] = lt.newList('ARRAY_LIST')
+    return channel
+
+
+def newID(name, id):
+    """
+    Esta estructura almancena los tags utilizados para marcar libros.
+    """
+    id = {'name': '', 'id': ''}
+    id['name'] = name
+    id['id'] = id
+    return id
 
 # Funciones de consulta
 
 # Funciones utilizadas para comparar elementos dentro de una lista
 
 # Funciones de ordenamiento
+
+def sortVideos(catalog):
+    sa.sort(catalog['videos'], compareviews)
