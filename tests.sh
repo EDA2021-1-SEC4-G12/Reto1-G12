289ae5e9d7c85f7297225d2aed182b86376008a3
Samuel <s.torresf@uniandes.edu.co>
2021-02-24 16:19:27 -0500

    fixing bugs

--- tests.sh
+++ tests.sh
@@ -1,23 +1,2 @@
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type shell
-
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type shell
-
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type shell
-
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type shell
-
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type shell
-
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type insertion
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type selection
-python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type shell
+python App/view_tests.py --type_list ARRAY_LIST 
+python App/view_tests.py --type_list SINGLE_LINKED 
