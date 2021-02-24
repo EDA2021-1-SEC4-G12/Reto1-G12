8dfdbc01c7cb655dd620f79f32b17cdbab956f67
Samuel <s.torresf@uniandes.edu.co>
2021-02-23 23:17:02 -0500

    .

---
+++ tests.sh
@@ -0,0 +1,23 @@
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 1000 --sort_type shell
+
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 2000 --sort_type shell
+
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 4000 --sort_type shell
+
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 8000 --sort_type shell
+
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 16000 --sort_type shell
+
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type insertion
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type selection
+python App/view_tests.py --type_list ARRAY_LIST --sample_size 32000 --sort_type shell
